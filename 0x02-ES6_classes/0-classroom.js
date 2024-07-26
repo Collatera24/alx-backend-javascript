1dbba8f1aa1b5bb27e0ff513509344e6bb847d9f
kola tella <kola.tella24@gmail.com>
2024-07-26 00:09:18 +0000

    Classes

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,14 @@
+/* eslint-disable */
+/**
+ * Implement a class named ClassRoom
+ */
+export default class ClassRoom {
+	/**
+	 * Creates a new @see {@link ClassRoom}.
+	 * @param {Number} maxStudentsSize - The maximum number of
+	 * students in the class.
+	 */
+	constructor(maxStudentsSize) {
+		this._maxStudentsSize = maxStudentsSize;
+	}
+}
