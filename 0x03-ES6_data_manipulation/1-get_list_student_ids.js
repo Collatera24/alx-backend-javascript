fe1ed1fe1360cfd07223e603deca9ea40f36f2d4
kola tella <kola.tella24@gmail.com>
2024-07-30 21:37:25 +0000

    Data Manipulation

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+
+/** 
+ * Gets id list of students.
+ * @param {{
+ * id: Number,
+ * firstName: String,
+ * location: String
+ * }[]} students - The list of students.
+ * @returns
+ */
+
+export default function getListStudentIds(students) {
+	if (students instanceof Array) {
+		return students.map((student) => student.id);
+	}
+	return [];
+}
+
