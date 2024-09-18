1589cd70064b87f7ce0c2b368242cb80a19771bd
Collatera24 <kola.tella24@gmail.com>
2024-09-18 23:35:38 +0100

    nodejs

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Kolawole Tella <https://github.com/Collatera24>
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+  };
+  
+  module.exports = displayMessage;
